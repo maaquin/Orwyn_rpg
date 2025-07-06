6fcf00bdbd4d38fc05dd25d0875d9ce5056377dc
maaquin <lucianomaquin@gmail.com>
2025-07-06 01:57:48 -0600

    create

--- pages/api/player/create.js
+++ pages/api/player/create.js
@@ -1,32 +1,31 @@
-import dbConnect from '../../../lib/mongoose';
-import Player from '../../../models/player';
+import dbConnect from '@/lib/mongoose';
+import Player from '@/models/player';
 
 export default async function handler(req, res) {
-    // Headers CORS siempre presentes
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-
-    // Manejar preflight CORS (muy importante)
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    console.log('create');
+    console.log('init create')
     await dbConnect();
     console.log("Método recibido:", req.method);
 
     if (req.method === 'POST') {
         try {
             const playerData = req.body;
+
             const newPlayer = await Player.create(playerData);
-            res.status(201).json({ message: "Success", playerId: newPlayer._id });
+
+            res.status(201).json({ message: "Succes", playerId: newPlayer._id });
         } catch (error) {
-            console.error("Error al crear jugador:", error);
+            if (error.name === 'ValidationError') {
+                console.error("Detalles de validación:");
+                for (const field in error.errors) {
+                    console.error(`- ${field}: ${error.errors[field].message}`);
+                }
+            } else {
+                console.error("Error general:", error);
+            }
+
             res.status(500).json({ message: "Error", error: error.message });
         }
     } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).json({ message: "Wrong method" });
+        res.status(405).json({ message: "Wrong metod" });
     }
 }
